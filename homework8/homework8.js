85fbc873fd9ee58266fb3cb514bd521017c2bb4d
DianaBondarenko <57017814+DianaBondarenko@users.noreply.github.com>
2020-12-10 12:52:19 +0200

    Add files via upload

--- homework8/homework8.js
+++ homework8/homework8.js
@@ -19,39 +19,44 @@ checkEmail("Qmail2@gmail.com");  // true
 
 
 //  task3
-let headers = document.querySelectorAll("h2.head");
-console.log(headers);
-for (let h of headers) {
-    h.style.background = "green";
-    let hInner = h.children;
-    for (hIn of hInner) {
-        if (hIn.className == "inner") {
-            hIn.style.fontSize = "35px";
-        }
-    }
-}
+// let headers = document.querySelectorAll("h2.head");
+// console.log(headers);
+// for (let h of headers) {
+//     h.style.background = "green";
+//     let hInner = h.children;
+//     for (hIn of hInner) {
+//         if (hIn.className == "inner") {
+//             hIn.style.fontSize = "35px";
+//         }
+//     }
+// }
 
+
+//  task 3
+let regexp = /d(b+)(d)/i;
+console.log( regexp.exec("cdbBdbsbz") );
+//console.log( "cdbBdbsbz".match(regexp) );
 
 //  task4
-let checkboxes = document.querySelectorAll("#checkboxes input");
-for (let i=0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", function(){
-        let n = 0;
-        for (let ch of checkboxes) {
-            if (ch.checked) {
-                n++;
-            }
-            if (n >= 3 ) {
-                for (let ch of checkboxes) {
-                    ch.disabled = true;
-                }
-            }
-        }
-  });
-}
+// let checkboxes = document.querySelectorAll("#checkboxes input");
+// for (let i=0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener("change", function(){
+//         let n = 0;
+//         for (let ch of checkboxes) {
+//             if (ch.checked) {
+//                 n++;
+//             }
+//             if (n >= 3 ) {
+//                 for (let ch of checkboxes) {
+//                     ch.disabled = true;
+//                 }
+//             }
+//         }
+//   });
+// }
 
 
-//  task5
+//  task
 function swap(str) {
     let arr = str.split(" ");
     return arr[1] + " " + arr[0];
@@ -59,7 +64,7 @@ function swap(str) {
 swap("Java Script");
 
 
-//  task6
+//  task5
 function checkCard(card) {
     let regexp = /\d{4}-?\d{4}-?\d{4}-?\d{4}/;
     return regexp.test(card);
@@ -67,9 +72,9 @@ function checkCard(card) {
 checkCard("9999-9999-9999-9999");  // true
 
 
-//  task7
+//  task6
 function checkEmail(email) {
-    let regexp =/^[A-Za-z0-9]+[A-Za-z0-9_]*-?[A-Za-z0-9_]*@[a-z0-9-]{2,}\.[a-z]{2,4}$/;
+    let regexp = /^[A-Za-z0-9]+[A-Za-z0-9_]*-?[A-Za-z0-9_]*@[a-z0-9-]{2,}\.[a-z]{2,4}$/;
     if (regexp.test(email)) {
         console.log("Email is correct!");
     } else {
@@ -78,3 +83,15 @@ function checkEmail(email) {
 }
 checkEmail("my_mail@gmail.com");  
 checkEmail("#my_mail@gmail.com");  
+
+
+//  task7
+function checkLogin(login) {
+    let regexp = /^[a-z][a-z0-9\.]{1,9}$/i
+    console.log( regexp.test(login) );
+    let regexp2 = /[0-9]\.[0-9]|[0-9]/g;
+    console.log( login.match(regexp2) );
+}
+
+checkLogin('ee1.1ret3');  // true  // ["1.1", "3"]
+checkLogin('ee1*1ret3');  // false  // ["1", "1", "3"]
